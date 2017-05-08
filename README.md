8dadd474e4cc2e61799c1ce02d8af727e3a7a41c
sorincovor <sorin_realsk8@yahoo.com>
2017-05-08 19:33:01 +0300

    added traffic light1

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # SE
-<h>Projet systemes d'exploitation</h> </br>
+<h1>Projet systemes d'exploitation</h1> </br>
 No.8: Simulation d'un carrefour urbain - voitures et feux, avec fils d'éxecution </br>
 Equipe: Covor Sorin, Stanescu Vlad et Rusu Bogdan</br>
--- traffic-simulation.py
+++ traffic-simulation.py
@@ -2,10 +2,18 @@ import Tkinter as tk
 import threading
 import time
 from threading import Thread
+from Tkinter import *
+
+light1 = False
+
+def changeLight():
+    global light1
+    light1 = not light1
 
 root=tk.Tk()
 root.title("Traffic Simulation")
-
+button = tk.Button(root, text='Stop', width=25, command=changeLight)
+button.pack()
 SIZE = 600
 
 canvas = tk.Canvas(root, width = SIZE, height = SIZE, bg = "#FFE19C")
@@ -25,23 +33,28 @@ for t in intersection_data:
     # canvas.create_line(x0, y0, x1, y1, ..., xn, yn, option, ...)
     canvas.create_line(t, width = 5)
 
+class Light(Thread, object):
+    def __init__():
+        Thread.__init__(self)
+
 class Car(Thread, object):
     def __init__(self, a,b,c,d, outline='blue', fill='blue'):
         Thread.__init__(self)
         self.rect = canvas.create_rectangle(a,b,c,d, outline=outline, fill=fill)
         self.speed = (0, 0)
     def move(self):
-        canvas.move(self.rect, self.speed[0], self.speed[1])
+        if(light1):
+            canvas.move(self.rect, self.speed[0], self.speed[1])
     def set_speed(self, x, y):
         self.speed = x, y
 
-car1 = Car(20, 155, 40, 170, outline='blue', fill='blue')
+car1 = Car(100, 100, 105, 105, outline='blue', fill='blue')
 car1.set_speed(5, 0)
 car2 = Car(20, 180, 40, 195, outline='red', fill = 'red')
 car2.set_speed(3, 0)
 
 # move cars
-for x in range(200):
+for x in range(2000):
     time.sleep(0.025)
     car1.move()
     car2.move()
